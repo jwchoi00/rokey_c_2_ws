7a652708c352af542ed3c0d1cb08cb542710731b
jwchoi00 <jwchoi0017@gmail.com>
2024-11-06 17:00:00 +0900

    G1 DB First

--- src/c2_turtle_order_package/setup.py
+++ src/c2_turtle_order_package/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'jiwon'
+package_name = 'c2_turtle_order_package'
 
 setup(
     name=package_name,
@@ -20,6 +20,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'order_saver = c2_turtle_order_package.order_saver:main',
+            'order_client = c2_turtle_order_package.order_client:main',
+            'total_sales_client = c2_turtle_order_package.total_sales_client:main'
         ],
     },
 )
